d1e234aab947345abc3f100279575ede088cfed1
Nakshatra Garg <nakshatra@kugelblitz.xyz>
2026-08-21 10:51:15 +0530

    kibana dashboard aka ELK

--- recipe-api/producer-http-basic.js
+++ recipe-api/producer-http-basic.js
@@ -1,36 +1,28 @@
 #!/usr/bin/env node
-
 const server = require('fastify')();
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 4000;
 
-console.log(`worker pid=${process.pid}`)
-
-server.get('/recipes/:id', async(req, reply) => {
-    console.log(`worker request pid=${process.pid}`)
-    const id = Number(req.params.id)
-    if (id !== 42) {
-        reply.code(404);
-        return { error: 'not_found' };
+server.get('/recipes/:id', async (req, reply) => {
+  const id = Number(req.params.id);
+  if (id !== 42) {
+    reply.statusCode = 404;
+    return { error: 'not_found' };
+  }
+  return {
+    producer_pid: process.pid,
+    recipe: {
+      id,
+      name: "Chicken Tikka Masala",
+      steps: "Throw it in a pot...",
+      ingredients: [
+        { id: 1, name: "Chicken", quantity: "1 lb" },
+        { id: 2, name: "Sauce", quantity: "2 cups" }
+      ]
     }
-    return {
-        producer_pid: process.pid,
-        recipe: {
-            id, name: "Chicken Tikka Masala",
-            steps: "Throw it in a pot...",
-            ingredients: [
-                { id: 1, name: "Chicken", quantity: "1 lb", },
-                { id: 2, name: "Sauce", quantity: "2 cups", }
-            ]
-        }
-    };
-})
+  };
+});
 
-server.listen({ port: PORT, host: HOST }, (err, address) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    }
-
-    console.log(`Producer running at ${address}`);
+server.listen(PORT, HOST, () => {
+  console.log(`Producer running at http://${HOST}:${PORT}`);
 });
